dd9a241fc508bdfa493dac4719fb723b22251d20
RappoyAthyaa <raffbrtd.18@gmail.com>
2025-10-28 14:52:54 +0700

    ini percobaan

--- ujicoba.java
+++ ujicoba.java
@@ -3,5 +3,6 @@ package PraktikumDaspro02;
 public class ujicoba {
     public static void main(String[] args) {
         System.out.println("test git");
+        System.out.println("hanya untuk mencoba saja");
     }
 }
